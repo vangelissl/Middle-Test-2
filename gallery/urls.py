bcbfd8dc6b2965b717257704d186068bbdc5f5d8
Maria Stashenko <mahryia@gmail.com>
2025-05-23 13:16:40 +0300

    Added test for gallery view

--- gallery/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import gallery_view, image_detail
-
-urlpatterns = [
-    path('', gallery_view, name='gallery'),
-    path('images/<int:id>/', image_detail, name='image_detail'),
-]
