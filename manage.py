9c28968ba82e108d1c500bce0dbb523384e06f7b
Maria Stashenko <mahryia@gmail.com>
2025-05-23 13:36:01 +0300

    Added db

--- manage.py
+++ manage.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
+from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
+    load_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_gallery.settings')
     try:
         from django.core.management import execute_from_command_line
